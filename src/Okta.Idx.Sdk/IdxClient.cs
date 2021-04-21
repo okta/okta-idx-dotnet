9b34f721d94d5b77bce90506b7a80d9add9de531
Andrii Zhegurov <68990515+andriizhegurov-okta@users.noreply.github.com>
2021-04-21 17:35:44 +0300

    Update src/Okta.Idx.Sdk/IdxClient.cs
    Co-authored-by: Laura Rodríguez <rdz.maria.laura@gmail.com>

--- src/Okta.Idx.Sdk/IdxClient.cs
+++ src/Okta.Idx.Sdk/IdxClient.cs
@@ -440,7 +440,7 @@ namespace Okta.Idx.Sdk
                                         .Recover
                                         .ProceedAsync(recoveryRequest, cancellationToken);
 
-            var recoveryrecoveryAuthenticators = recoveryResponse
+            var recoveryAuthenticators = recoveryResponse
                 .Authenticators
                 .Value;
 
