f3a8e437695344165cf736ce8f56c6b3097ae64a
Bryan Apellanes bryan.apellanes@okta.com <bryan.apellanes@okta.com>
2021-05-10 08:08:59 -0700

    add href to idpoption

--- src/Okta.Idx.Sdk/IdpOption.cs
+++ src/Okta.Idx.Sdk/IdpOption.cs
@@ -10,6 +10,8 @@ namespace Okta.Idx.Sdk
 
         public string InteractionHandle { get; set; }
 
+        public string Href { get; set; }
+
         public string Id { get; set; }
 
         public string Name { get; set; }
