10e3d3678696acd5a5a626484b43c44876e2713c
Bryan Apellanes bryan.apellanes@okta.com <bryan.apellanes@okta.com>
2021-05-13 08:09:16 -0700

    documentation comments for IdxUrlHelper

--- src/Okta.Idx.Sdk/IdxUrlHelper.cs
+++ src/Okta.Idx.Sdk/IdxUrlHelper.cs
@@ -5,9 +5,17 @@ using System.Text;
 
 namespace Okta.Idx.Sdk
 {
+    /// <summary>
+    /// Static url helper methods relevant to Idx.
+    /// </summary>
     public static class IdxUrlHelper
     {
-
+        /// <summary>
+        /// Gets a normalized URI string for the specified issuer and resource.
+        /// </summary>
+        /// <param name="issuer">The issuer.</param>
+        /// <param name="resourceUri">The resource URI.</param>
+        /// <returns>Normalied URI string.</returns>
         public static string GetNormalizedUriString(string issuer, string resourceUri)
         {
             string normalized = issuer;
@@ -23,6 +31,11 @@ namespace Okta.Idx.Sdk
             return normalized;
         }
 
+        /// <summary>
+        /// Determines if the specified issuer URI is the root issuer for the organization.
+        /// </summary>
+        /// <param name="issuerUri">The issuer URI.</param>
+        /// <returns>bool</returns>
         public static bool IsRootOrgIssuer(string issuerUri)
         {
             string path = new Uri(issuerUri).AbsolutePath;
