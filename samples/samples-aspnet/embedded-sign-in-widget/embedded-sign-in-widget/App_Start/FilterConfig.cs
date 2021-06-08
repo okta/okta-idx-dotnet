cd9d0c649dffaeafe3212b10fc40ea6e34cc82cb
Bryan Apellanes <63638027+bryanapellanes-okta@users.noreply.github.com>
2021-06-07 18:38:38 -0700

    Embedded widget sample (#59)

---
+++ samples/samples-aspnet/embedded-sign-in-widget/embedded-sign-in-widget/App_Start/FilterConfig.cs
@@ -0,0 +1,13 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace embedded_sign_in_widget
+{
+    public class FilterConfig
+    {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            filters.Add(new HandleErrorAttribute());
+        }
+    }
+}
---
+++ samples/samples-aspnet/embedded-sign-in-widget/embedded-sign-in-widget/Startup.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartup(typeof(embedded_sign_in_widget.Startup))]
+
+namespace embedded_sign_in_widget
+{
+#pragma warning disable SA1601 // Partial elements should be documented
+    public partial class Startup
+#pragma warning restore SA1601 // Partial elements should be documented
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
