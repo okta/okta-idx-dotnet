742e1a885bea91cb0b6a43e74c989cdcb674eb95
Andrii Zhegurov <andrii.zhegurov@okta.com>
2021-12-07 17:43:02 +0200

    WIP

---
+++ samples/samples-aspnet/embedded-auth-with-sdk/embedded-auth-with-sdk/Models/EnrollGoogleAuthenticatorViewModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace embedded_auth_with_sdk.Models
+{
+    public class EnrollGoogleAuthenticatorViewModel : BaseViewModel
+    {
+
+        [Display(Name = "QR code")]
+        public string QrCodeHref { get; set; }
+
+        [Display(Name = "Shared secret")]
+        public string SharedSecret { get; set; }
+    }
+}
