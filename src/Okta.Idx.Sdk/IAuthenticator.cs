7a4eb64b6f7ee6c299634b3776efa3f23cfd133f
Bryan Apellanes <bryan.apellanes@okta.com>
2022-01-19 09:16:02 -0800

    remove unnecessary properties

--- src/Okta.Idx.Sdk/IAuthenticator.cs
+++ src/Okta.Idx.Sdk/IAuthenticator.cs
@@ -47,16 +47,5 @@ namespace Okta.Idx.Sdk
         /// Gets or sets the credential Id. This is only applicable for Web Authn authenticators.
         /// </summary>
         string CredentialId { get; set; }
-
-        /// <summary>
-        /// Gets or sets the QrCode.
-        /// This is applicable to TOTP authenticators such as Google Authenticator or Okta Verify.
-        /// </summary>
-        IQrCode QrCode { get; set; }
-
-        /// <summary>
-        /// Gets or sets the selected channel for Okta Verify.
-        /// </summary>
-        string SelectedChannel { get; set; }
     }
 }
