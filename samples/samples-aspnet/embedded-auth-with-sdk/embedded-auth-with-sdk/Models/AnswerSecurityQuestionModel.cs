654e2a7ad9287330a347ee4aa318f8652675b221
Bryan Apellanes <bryan.apellanes@okta.com>
2022-04-05 06:17:37 -0700

    cleanup model

--- samples/samples-aspnet/embedded-auth-with-sdk/embedded-auth-with-sdk/Models/AnswerSecurityQuestionModel.cs
+++ samples/samples-aspnet/embedded-auth-with-sdk/embedded-auth-with-sdk/Models/AnswerSecurityQuestionModel.cs
@@ -14,9 +14,6 @@ namespace embedded_auth_with_sdk.Models
         {
         }
 
-        public string QuestionKey { get; set; }
         public string Question { get; set; }
-        public string Answer { get; set; }
-
     }
 }
