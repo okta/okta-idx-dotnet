6ab34f427faac8317b328f8d8921746839e09456
Bryan Apellanes <bryan.apellanes@okta.com>
2022-04-05 06:26:21 -0700

    docs

--- src/Okta.Idx.Sdk/ISecurityQuestion.cs
+++ src/Okta.Idx.Sdk/ISecurityQuestion.cs
@@ -1,13 +1,25 @@
-﻿using Okta.Sdk.Abstractions;
-using System;
-using System.Collections.Generic;
-using System.Text;
+﻿// <copyright file="ISecurityQuestion.cs" company="Okta, Inc">
+// Copyright (c) 2020 - present Okta, Inc. All rights reserved.
+// Licensed under the Apache 2.0 license. See the LICENSE file in the project root for full license information.
+// </copyright>
+
+using Okta.Sdk.Abstractions;
 
 namespace Okta.Idx.Sdk
 {
+    /// <summary>
+    /// Represents a security question.
+    /// </summary>
     public interface ISecurityQuestion : IResource
     {
+        /// <summary>
+        /// Gets the question key.
+        /// </summary>
         string QuestionKey { get; }
+
+        /// <summary>
+        /// Gets the question.
+        /// </summary>
         string Question { get; }
     }
 }
