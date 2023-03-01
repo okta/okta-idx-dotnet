9226b06ea68c6c071aa69a8c7d96b1109436a47d
Bryan Apellanes <bryan.apellanes@okta.com>
2023-03-01 08:31:10 -0600

    add assertions

--- src/Okta.Idx.Sdk.UnitTests/SignInWidgetConfigurationShould.cs
+++ src/Okta.Idx.Sdk.UnitTests/SignInWidgetConfigurationShould.cs
@@ -16,8 +16,8 @@ namespace Okta.Idx.Sdk.UnitTests
     ""interactionHandle"": ""notreal"",
     ""version"": ""5.8.1"",
     ""baseUrl"": ""https://test.com"",
-    ""clientId"": ""0oa738lmiljOyCtEn1d7"",
-    ""redirectUri"": ""https://localhost:51936/interactioncode/callback"",
+    ""clientId"": ""testclientid"",
+    ""redirectUri"": ""testredirecturi"",
     ""authParams"": {
         ""issuer"": ""https://test.com"",
         ""scopes"": [
@@ -28,12 +28,29 @@ namespace Okta.Idx.Sdk.UnitTests
         ]
     },
     ""useInteractionCodeFlow"": true,
-    ""state"": ""dyR1gauT_RxOP299Bn9q1A"",
-    ""codeChallenge"": ""bqKBsjNt4TsV-p9NUcZvKpFN-g5VT_uMHKVy72d6fGI"",
+    ""state"": ""teststate"",
+    ""codeChallenge"": ""testcodechallenge"",
     ""codeChallengeMethod"": ""S256""
 }";
             var signInWidgetConfiguratin = JsonConvert.DeserializeObject<SignInWidgetConfiguration>(signInWidgetJson);
             signInWidgetConfiguratin.Should().NotBeNull();
+            signInWidgetConfiguratin.InteractionHandle.Should().Be("notreal");
+            signInWidgetConfiguratin.Version.Should().Be("5.8.1");
+            signInWidgetConfiguratin.BaseUrl.Should().Be("https://test.com");
+            signInWidgetConfiguratin.ClientId.Should().Be("testclientid");
+            signInWidgetConfiguratin.RedirectUri.Should().Be("testredirecturi");
+            signInWidgetConfiguratin.AuthParams.Should().NotBeNull();
+            signInWidgetConfiguratin.AuthParams.Issuer.Should().Be("https://test.com");
+            signInWidgetConfiguratin.AuthParams.Scopes.Should().NotBeNull();
+            List<string> scopes = new List<string>(signInWidgetConfiguratin.AuthParams.Scopes);
+            scopes.Contains("openid").Should().BeTrue();
+            scopes.Contains("profile").Should().BeTrue();
+            scopes.Contains("email").Should().BeTrue();
+            scopes.Contains("offline_access").Should().BeTrue();
+            signInWidgetConfiguratin.UseInteractionCodeFlow.Should().BeTrue();
+            signInWidgetConfiguratin.State.Should().Be("teststate");
+            signInWidgetConfiguratin.CodeChallenge.Should().Be("testcodechallenge");
+            signInWidgetConfiguratin.CodeChallengeMethod.Should().Be("S256");
         }
     }
 }
