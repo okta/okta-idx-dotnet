ab5f8ff5f944b03c1e3f7004ba247aa2d2649549
Bryan Apellanes <bryan.apellanes@okta.com>
2023-03-19 08:31:10 -0500

    fix typo

--- src/Okta.Idx.Sdk.UnitTests/SignInWidgetConfigurationShould.cs
+++ src/Okta.Idx.Sdk.UnitTests/SignInWidgetConfigurationShould.cs
@@ -32,25 +32,25 @@ namespace Okta.Idx.Sdk.UnitTests
     ""codeChallenge"": ""testcodechallenge"",
     ""codeChallengeMethod"": ""S256""
 }";
-            var signInWidgetConfiguratin = JsonConvert.DeserializeObject<SignInWidgetConfiguration>(signInWidgetJson);
-            signInWidgetConfiguratin.Should().NotBeNull();
-            signInWidgetConfiguratin.InteractionHandle.Should().Be("notreal");
-            signInWidgetConfiguratin.Version.Should().Be("5.8.1");
-            signInWidgetConfiguratin.BaseUrl.Should().Be("https://test.com");
-            signInWidgetConfiguratin.ClientId.Should().Be("testclientid");
-            signInWidgetConfiguratin.RedirectUri.Should().Be("testredirecturi");
-            signInWidgetConfiguratin.AuthParams.Should().NotBeNull();
-            signInWidgetConfiguratin.AuthParams.Issuer.Should().Be("https://test.com");
-            signInWidgetConfiguratin.AuthParams.Scopes.Should().NotBeNull();
-            List<string> scopes = new List<string>(signInWidgetConfiguratin.AuthParams.Scopes);
+            var signInWidgetConfiguration = JsonConvert.DeserializeObject<SignInWidgetConfiguration>(signInWidgetJson);
+            signInWidgetConfiguration.Should().NotBeNull();
+            signInWidgetConfiguration.InteractionHandle.Should().Be("notreal");
+            signInWidgetConfiguration.Version.Should().Be("5.8.1");
+            signInWidgetConfiguration.BaseUrl.Should().Be("https://test.com");
+            signInWidgetConfiguration.ClientId.Should().Be("testclientid");
+            signInWidgetConfiguration.RedirectUri.Should().Be("testredirecturi");
+            signInWidgetConfiguration.AuthParams.Should().NotBeNull();
+            signInWidgetConfiguration.AuthParams.Issuer.Should().Be("https://test.com");
+            signInWidgetConfiguration.AuthParams.Scopes.Should().NotBeNull();
+            List<string> scopes = new List<string>(signInWidgetConfiguration.AuthParams.Scopes);
             scopes.Contains("openid").Should().BeTrue();
             scopes.Contains("profile").Should().BeTrue();
             scopes.Contains("email").Should().BeTrue();
             scopes.Contains("offline_access").Should().BeTrue();
-            signInWidgetConfiguratin.UseInteractionCodeFlow.Should().BeTrue();
-            signInWidgetConfiguratin.State.Should().Be("teststate");
-            signInWidgetConfiguratin.CodeChallenge.Should().Be("testcodechallenge");
-            signInWidgetConfiguratin.CodeChallengeMethod.Should().Be("S256");
+            signInWidgetConfiguration.UseInteractionCodeFlow.Should().BeTrue();
+            signInWidgetConfiguration.State.Should().Be("teststate");
+            signInWidgetConfiguration.CodeChallenge.Should().Be("testcodechallenge");
+            signInWidgetConfiguration.CodeChallengeMethod.Should().Be("S256");
         }
     }
 }
